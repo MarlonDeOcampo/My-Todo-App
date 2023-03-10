79ac4c0c44488fd673d6bd1fb6cfe01912341233
Marlon <marlon.deocampo@shipteksolutions.com>
2023-03-11 01:23:44 +0800

    fixed logout

--- src/componets/layout/navbar.tsx
+++ src/componets/layout/navbar.tsx
@@ -1,12 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
   const navigate = useNavigate();
   return (
     <nav className="h-14 w-full flex flex-row justify-between items-center fixed-top px-20 bg-secondary text-white">
       <div className="text-3xl font-bold ">Logo</div>
       <button className="font-semibold" onClick={() => navigate("/")}>
-        Logout
+        {location.pathname === "/" ? null : <p>Logout</p>}
       </button>
     </nav>
   );
